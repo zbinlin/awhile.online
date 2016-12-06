45b5f65b1b40afc3f25ad1499c0acf58122d31e2
Colin Zheng <zbinlin@gmail.com>
2016-12-06 13:28:20 +0800

    {REFACTOR(server)}: anti xss

--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,30 @@
 "use strict";
 
+const HTML_ESCAPED_CHARS = {
+    "<" : "&lt;",
+    ">" : "&gt;",
+    "\"": "&quot;",
+    "&" : "&amp;",
+    "'" : "&#39;",
+    "/" : "&#47;",
+    "`" : "&#96;",
+};
+const HTML_UNSAFE_CHARS_REGEXP = /[<>"\\&'\/`]/g;
+
+const JS_ESCAPED_CHARS = {
+    "<"     : "\\u003C",
+    ">"     : "\\u003E",
+    "/"     : "\\u002F",
+    "\u2028": "\\u2028",
+    "\u2029": "\\u2029",
+};
+const JS_UNSAFE_CHARS_REGEXP = /[<>\/\u2028\u2029]/g;
+
 export function outputToHTML(str) {
-    return str.replace(/<>\//g, $0 => {
-        switch ($0) {
-            case "<":
-                return "&lt;";
-            case ">":
-                return "&gt;";
-            default:
-                return `&#${$0.charCodeAt(0)};`;
-        }
-    });
+    return str.replace(HTML_UNSAFE_CHARS_REGEXP, ch => HTML_ESCAPED_CHARS[ch]);
 }
 
 export function outputToJS(str) {
-    return JSON.stringify(str);
+    return JSON.stringify(str)
+        .replace(JS_UNSAFE_CHARS_REGEXP, ch => JS_ESCAPED_CHARS[ch]);
 }
