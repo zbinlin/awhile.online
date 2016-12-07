ab2615c748e42257710b571bf22a8424bde0e103
Colin Zheng <zbinlin@gmail.com>
2016-12-07 21:00:40 +0800

    {CHORE(server)}: change email length to 320

--- server/user.sql
+++ server/user.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS awhile_users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(32) UNIQUE NOT NULL,
     nickname VARCHAR(32),
-    email VARCHAR(128),
+    email VARCHAR(320),
     password CHAR(1024) NOT NULL,
     salt CHAR(256) NOT NULL
 );
